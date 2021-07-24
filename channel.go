f5abffee7e4b27b2d0629e87761bde81a8069595
jrFlint <flint@77i.su>
2021-07-24 10:11:48 +0300

    in progress

--- channel.go
+++ channel.go
@@ -1,5 +1,39 @@
 package monitoring
 
+import (
+	"fmt"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
 type Channel interface {
 	Log(title, message string)
+}
+
+// channel telegram
+
+type ChannelTelegram struct {
+	botAPI *tgbotapi.BotAPI
+	ChatID int64
+}
+
+func (s *ChannelTelegram) Log(title, message string) {
+	chatMessage := fmt.Sprintf(`<b>%v</b>\n%v`, title, message)
+	tMsg := tgbotapi.NewMessage(s.ChatID, chatMessage)
+	s.botAPI.Send(tMsg)
+}
+
+func (s *ChannelTelegram) JSONField(fieldName string) (ptr interface{}, err error) {
+	var token string
+	switch fieldName {
+	case "token":
+		ptr = &token
+	case "chat_id":
+		ptr = &s.ChatID
+	}
+	return
+}
+
+func (s *ChannelTelegram) JSONFinish() (err error) {
+	if 
 }
