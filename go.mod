e3cee67d5b41a8910c84b5943c051ba61118f167
Vladimir V Maksimov <flint@77i.su>
2021-07-26 01:16:24 +0300

    in progress

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/fcg-xvii/monitoring
 go 1.16
 
 require (
-	github.com/fcg-xvii/go-tools v0.0.0-20210724151639-bf080e8ec684
+	github.com/fcg-xvii/go-tools v0.0.0-20210725221359-597a6da77a03
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
--- monitoring.go
+++ monitoring.go
@@ -5,13 +5,18 @@ import (
 	"log"
 	"time"
 
+	"github.com/fcg-xvii/go-tools/containers/concurrent"
 	"github.com/fcg-xvii/go-tools/json"
 )
 
 func New(ctx context.Context, objects []Object, interval time.Duration) *Monitoring {
 	cctx, cancel := context.WithCancel(ctx)
+	l := concurrent.NewList()
+	for _, obj := range objects {
+		l.PushBack(obj)
+	}
 	m := &Monitoring{
-		objects:   objects,
+		objects:   l,
 		ctx:       cctx,
 		cancel:    cancel,
 		mInterval: interval,
@@ -24,7 +29,7 @@ func FromMap(ctx context.Context, m json.Map) *Monitoring {
 	items := m.Slice("monitoring", nil)
 	res := &Monitoring{
 		mInterval: time.Minute * time.Duration(m.Int("interval", 5)),
-		objects: make([]Object, 0, len(items))
+		objects:   concurrent.NewList(),
 	}
 	for _, item := range items {
 		if iMap, check := item.(map[string]interface{}); check {
@@ -45,7 +50,7 @@ func FromConfig(ctx context.Context, fileName string) (res *Monitoring, err erro
 }
 
 type Monitoring struct {
-	objects   []Object
+	objects   *concurrent.List
 	ctx       context.Context
 	cancel    context.CancelFunc
 	mInterval time.Duration
@@ -55,14 +60,25 @@ func (s *Monitoring) Cancel() {
 	s.cancel()
 }
 
+func (s *Monitoring) ObjectAdd(obj Object) {
+	s.objects.PushBack(obj)
+}
+
+func (s *Monitoring) ObjectRemove(obj Object) {
+	elem := s.objects.Search(obj)
+	if elem != nil {
+		s.objects.Remove(elem)
+	}
+}
+
 func (s *Monitoring) start() {
 	mTimer := time.NewTicker(s.mInterval)
 	for {
 		select {
 		case <-mTimer.C:
 			{
-				for _, obj := range s.objects {
-					obj.Request()
+				for f := s.objects.First(); f != nil; f = f.Next() {
+					f.Val().(Object).Request()
 				}
 			}
 		case <-s.ctx.Done():
